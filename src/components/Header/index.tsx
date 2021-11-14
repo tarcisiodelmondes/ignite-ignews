6566ad2cabd754fadf4606ac03d428ec241ea884
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-11-14 18:20:12 -0300

    Component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import { SignInButton } from "../../SignInButton";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image
+          src="/images/logo.svg"
+          alt="Logo ig.news"
+          width="108px"
+          height="30px"
+        />
+
+        <nav>
+          <a className={styles.active} href="#">
+            Home
+          </a>
+          <a href="#">Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
