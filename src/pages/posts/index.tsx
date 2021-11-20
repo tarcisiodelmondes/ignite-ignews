6f82e2f26ca6467bf27e2ca367a1a3478339950a
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-11-20 20:03:03 -0300

    Pagina post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from "next";
 import { getPrismicClient } from "../../services/prismic";
 
 import Prismic from "@prismicio/client";
+import Link from "next/link";
 import Head from "next/head";
 
 import styles from "./styles.module.scss";
@@ -28,11 +29,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
